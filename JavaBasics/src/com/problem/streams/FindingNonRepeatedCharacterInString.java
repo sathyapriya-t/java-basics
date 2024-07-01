3728271290c66ce92df61ef57048080acadd33d3
sathya.Priya <sathya.Priya@sainsburys.co.uk>
2024-07-01 14:26:05 +0530

    formatted

--- JavaBasics/src/com/problem/streams/FindingNonRepeatedCharacterInString.java
+++ JavaBasics/src/com/problem/streams/FindingNonRepeatedCharacterInString.java
@@ -11,7 +11,7 @@ public class FindingNonRepeatedCharacterInString {
 	public static void main(String[] args) {
 		// Given a String,
 		// find the first non-repeated character in it using Stream functions?
-		
+
 		String str = "SathyaArun";
 		nonRepeatedCharUsingMapAndCounting(str);
 		usingFunctionIdentity(str);
@@ -19,9 +19,8 @@ public class FindingNonRepeatedCharacterInString {
 
 	private static void usingFunctionIdentity(String str) {
 		Character collect = str.chars().mapToObj(c -> Character.toLowerCase((char) c))
-				.collect(Collectors.groupingBy(Function.identity(),Collectors.counting()))
-				.entrySet().stream().filter(entry -> entry.getValue() == 1)
-				.map(val -> val.getKey()).findFirst().get();
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet().stream()
+				.filter(entry -> entry.getValue() == 1).map(val -> val.getKey()).findFirst().get();
 		System.out.println("nonRepeatedStr: " + collect);
 	}
 
@@ -29,12 +28,12 @@ public class FindingNonRepeatedCharacterInString {
 		String[] split = str.toLowerCase().split("");
 		List<String> listStr = new ArrayList<String>();
 		Collections.addAll(listStr, split);
-		List<String> nonRepeatedStr = listStr.stream()
-				.collect(Collectors.groupingBy(s->s,Collectors.counting()))
-				.entrySet().stream().filter(val -> val.getValue()==1)
-				.collect(Collectors.mapping(result -> result.getKey(),
-						Collectors.toList()));
-		System.out.println("nonRepeatedStr: " +nonRepeatedStr);
+		List<String> nonRepeatedStr = listStr.stream().collect(Collectors.groupingBy(s -> s, Collectors.counting()))
+				.entrySet().stream().filter(val -> val.getValue() == 1)
+				.collect(Collectors.mapping(result -> result.getKey(), Collectors.toList()));
+		System.out.println("nonRepeatedStr: " + nonRepeatedStr);
 	}
 
 }
+
+
