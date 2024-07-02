c152eab45c715617c395d25d37fa2164969d2bfb
sathyapriya-t <sathya2001t@gmail.com>
2024-07-02 17:02:53 +0530

    DateTimeApi - LocalDateTime and Conversions

---
+++ JavaBasics/src/com/java8/datetimeapi/LocalDateTimeImpl.java
@@ -0,0 +1,47 @@
+package com.java8.datetimeapi;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+
+public class LocalDateTimeImpl {
+
+	public static void main(String[] args) {
+		
+		//Creation
+		LocalDateTime localDateTime = LocalDateTime.now();
+		System.out.println("localDateTime : " +localDateTime);
+		LocalDateTime dateTime = LocalDateTime.of(2021, 12, 03, 10, 21);
+		System.out.println("dateTime : "+dateTime);
+		
+		//Getting values
+		System.out.println();
+		System.out.println("Getting values!! ");
+		System.out.println("getDayOfMonth : "+dateTime.getDayOfMonth());
+		System.out.println("getDayOfYear : "+dateTime.getDayOfYear());
+		System.out.println("getMonthValue : "+dateTime.getMonthValue());
+		System.out.println("getMinute : "+dateTime.getMinute());
+		System.out.println("getDayOfWeek : "+dateTime.getDayOfWeek());
+		System.out.println("get(ChronoField.DAY_OF_WEEK) : "+dateTime.get(ChronoField.DAY_OF_WEEK));
+		System.out.println("get(ChronoField.DAY_OF_MONTH) : "+dateTime.get(ChronoField.DAY_OF_MONTH));
+		System.out.println("getMonth : "+dateTime.getMonth());
+
+		//Modify DateTime
+		System.out.println();
+		System.out.println("Modify DateTime!! ");
+		System.out.println("dateTime add 10 DECADES: "+dateTime.plus(10, ChronoUnit.DECADES));
+		System.out.println("dateTime add 10 plusDays: "+dateTime.plusDays(10));
+		System.out.println("dateTime add 200 plusYears: "+dateTime.plusYears(200));
+		System.out.println("dateTime minus HALF_DAYS: "+dateTime.minus(10, ChronoUnit.HALF_DAYS));
+		System.out.println("dateTime minusMinutes: "+dateTime.minusMinutes(100));
+		System.out.println("dateTime minusMonths: "+dateTime.minusMonths(20));
+		
+		//Supporting Methods
+		System.out.println();
+		System.out.println("Supporting Methods !! ");
+		System.out.println("dateTime isAfter : "+dateTime.isAfter(localDateTime));
+		System.out.println("dateTime isSupported : "+dateTime.isSupported(ChronoField.DAY_OF_MONTH));
+		
+	}
+
+}
--- JavaBasics/src/com/java8/datetimeapi/LocalTimeImpl.java
+++ JavaBasics/src/com/java8/datetimeapi/LocalTimeImpl.java
@@ -5,46 +5,48 @@ import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
 
 public class LocalTimeImpl {
-	
+
 	public static void main(String[] args) {
-		
-		//Creation
+
+		// Creation
 		LocalTime time = LocalTime.now();
 		System.out.println("time " + time);
 		LocalTime time1 = LocalTime.of(12, 3);
-		System.out.println("time1 " +time1);
-		LocalTime time2 = LocalTime.of(12, 3,8);
-		System.out.println("time2 " +time2);
-		LocalTime time3 = LocalTime.of(12, 3,8,986960);
-		System.out.println("time3 " +time3);
-		
-		//Getting the values
+		System.out.println("time1 " + time1);
+		LocalTime time2 = LocalTime.of(12, 3, 8);
+		System.out.println("time2 " + time2);
+		LocalTime time3 = LocalTime.of(12, 3, 8, 986960);
+		System.out.println("time3 " + time3);
+
+		// Getting the values
 		System.out.println();
 		System.out.println("Getting the values!!");
-		System.out.println("time getHour" +time3.getHour());
-		System.out.println("time getHour" +time3.getMinute());
-		System.out.println("time getSecond" +time3.getSecond());
-		System.out.println("time getNano" +time3.getNano());
-		System.out.println("time getHour with chrono/Temporal field " +time3.get(ChronoField.HOUR_OF_DAY));
-		//System.out.println("time getHour with chrono/Temporal field " +time3.get(ChronoField.DAY_OF_WEEK)); //Throws Exception
-		System.out.println("time getHour with chrono/Temporal field " +time3.getLong(ChronoField.CLOCK_HOUR_OF_DAY));
+		System.out.println("time getHour" + time3.getHour());
+		System.out.println("time getHour" + time3.getMinute());
+		System.out.println("time getSecond" + time3.getSecond());
+		System.out.println("time getNano" + time3.getNano());
+		System.out.println("time getHour with chrono/Temporal field " + time3.get(ChronoField.HOUR_OF_DAY));
 		
-		//Modifing the values
+		
+		// System.out.println("time getHour with chrono/Temporal field "
+		// +time3.get(ChronoField.DAY_OF_WEEK)); //Throws Exception
+		System.out.println("time getHour with chrono/Temporal field " + time3.getLong(ChronoField.CLOCK_HOUR_OF_DAY));
+
+		// Modifing the values
 		System.out.println();
 		System.out.println("Modify the date!!");
-		System.out.println("time .minus(0,ChronoUnit.HOURS) =" +time3.minus(0,ChronoUnit.HOURS));
-		System.out.println("time plusHours =" +time3.plusHours(2));
-		System.out.println("time .plusMinutes =" +time3.plusMinutes(12));
-		System.out.println("time .plusNanos =" +time3.plusNanos(76485984));
-		System.out.println("time3.range(ChronoField.HOUR_OF_DAY) =" +time3.range(ChronoField.HOUR_OF_DAY));
+		System.out.println("time .minus(0,ChronoUnit.HOURS) =" + time3.minus(0, ChronoUnit.HOURS));
+		System.out.println("time plusHours =" + time3.plusHours(2));
+		System.out.println("time .plusMinutes =" + time3.plusMinutes(12));
+		System.out.println("time .plusNanos =" + time3.plusNanos(76485984));
+		System.out.println("time3.range(ChronoField.HOUR_OF_DAY) =" + time3.range(ChronoField.HOUR_OF_DAY));
 
-		//Aditional Supporting methods
+		// Aditional Supporting methods
 		System.out.println();
 		System.out.println("Aditional Supporting methods");
 		System.out.println(time3.isAfter(time2));
 		System.out.println(time3.isBefore(time2));
 		System.out.println(time3.isSupported(ChronoUnit.HOURS));
-		
 
 	}
 
