727d221f8f3493406ce90f1849d291c4b248d38e
sathyapriya-t <sathya2001t@gmail.com>
2024-07-09 23:26:03 +0530

    Throwing Exception updated

--- JavaBasics/src/com/exception/ThrowingExceptionImpl.java
+++ JavaBasics/src/com/exception/ThrowingExceptionImpl.java
@@ -10,11 +10,12 @@ class Amount {
 		this.amount = amount;
 	}
 
-	public void add(Amount that) {
+	// If it is unchecked exception we have to throw it
+	public void add(Amount that) throws Exception {
 		if (!this.currency.equals(that.currency)) {
 
 			// Throwing Exception
-			throw new RuntimeException("Currency is not Equal");
+			throw new Exception("Currency is not Equal" + " " + this.currency + " and " + that.currency);
 		}
 		this.amount = this.amount + that.amount;
 	}
@@ -27,9 +28,11 @@ class Amount {
 
 public class ThrowingExceptionImpl {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Amount amount = new Amount("USD", 12);
 		Amount amount1 = new Amount("INR", 12);
+
+		// Here either we can throw exception in declaration or handle it
 		amount.add(amount1);
 		System.out.println(amount);
 	}
