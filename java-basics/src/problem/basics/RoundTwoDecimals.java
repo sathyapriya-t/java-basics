dbffbb18c59c796ba924a9d4a3ba475c3c13d9c5
sathya.Priya <sathya2001t@gmail.com>
2024-07-14 18:47:00 +0530

    Basics - NumberFormat and DecimalFormat

--- java-basics/src/problem/basics/RoundTwoDecimals.java
+++ java-basics/src/problem/basics/RoundTwoDecimals.java
@@ -3,6 +3,7 @@ package problem.basics;
 public class RoundTwoDecimals {
 
 	public static void main(String[] args) {
+		
 		// Rounding a double value to two decimals
 		// multiply and divide by 100 -> this will return double value of two decimals
 		System.out.println("roundNumber with decimal: " + roundNumber(3.12));
