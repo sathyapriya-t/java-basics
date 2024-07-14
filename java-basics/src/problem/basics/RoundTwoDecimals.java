9dc5c0437ba1c3fe2fcde170b384c0253c93f16f
sathya.Priya <sathya2001t@gmail.com>
2024-07-14 17:41:47 +0530

    problem - RoundNumberWithTwoDecimals

---
+++ java-basics/src/problem/basics/RoundTwoDecimals.java
@@ -0,0 +1,15 @@
+package problem.basics;
+
+public class RoundTwoDecimals {
+
+	public static void main(String[] args) {
+		// Rounding a double value to two decimals
+		// multiply and divide by 100 -> this will return double value of two decimals
+		System.out.println("roundNumber with decimal: " + roundNumber(3.12));
+	}
+
+	static double roundNumber(double value) {
+		return Math.round(value * 100) / 100d;
+	}
+
+}
