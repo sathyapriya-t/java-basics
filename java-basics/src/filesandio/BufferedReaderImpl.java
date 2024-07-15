87cbd4f892084b8498a348425f76caef0c60f68f
sathyapriya-t <sathya2001t@gmail.com>
2024-07-16 01:14:12 +0530

    I/O - Buffered Reader

--- java-basics/src/filesandio/BufferedReaderImpl.java
+++ java-basics/src/filesandio/BufferedReaderImpl.java
@@ -1,23 +1,30 @@
 package filesandio;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class BufferedReaderImpl {
 
+	//reading and writing file using Buffered Reader and writer
 	public static void main(String[] args) {
-		File file = new File("./java-basics/src/resources/FileRead.txt");
+		File source = new File("./src/FileRead.txt");
+		File dest = new File("/JavaBasics/src/resources/sample.txt");
+		
 		String lines;
-		try (BufferedReader reader = new BufferedReader(new FileReader(file))){
+		try (BufferedReader reader = new BufferedReader(new FileReader(source));
+				BufferedWriter writer = new BufferedWriter(new FileWriter(dest))){
 			while ((lines = reader.readLine())!= null) {
-				System.out.println("lines:"+ lines);
+				writer.write(lines+"\n");
 		}} 
 		catch (IOException e) {
 			e.printStackTrace();
 		}
-	
+		
 	}
+	
 
 }
---
+++ java-basics/src/filesandio/ReadAndWriteByteFiles.java
@@ -0,0 +1,36 @@
+package filesandio;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/*
+Basically there are 2 types of file reading and writing
+
+one with Files.read() -> from java8
+Normal one with reader/ write(char)  and inputstream/outputstream (byte)
+
+reader/ writer - if end of the file read method returns null
+nputstream/outputstream - if end of the file read returns -1
+
+*/
+public class ReadAndWriteByteFiles {
+
+	public static void main(String[] args) {
+		File source = new File("./java-basics/src/resources/FileRead.txt");
+		File dest = new File("./java-basics/src/resources/ByteFileWrite.txt");
+
+		try (FileOutputStream outputStream = new FileOutputStream(dest);
+				FileInputStream inputStream = new FileInputStream(source)) {
+			int val;
+//			while ((val = inputStream.read()) != -1) {
+//				outputStream.write(val);
+//			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
