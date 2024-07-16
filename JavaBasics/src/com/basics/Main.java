f390d367e9209b84356f7734fa04fd6f5ca7488d
sathyapriya-t <sathya2001t@gmail.com>
2024-07-16 16:45:44 +0530

    Adding Main

---
+++ JavaBasics/src/com/basics/Main.java
@@ -0,0 +1,7 @@
+package com.basics;
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ JavaBasics/src/com/oops/simpleinterest/SimpleInterest.java
@@ -0,0 +1,33 @@
+package com.oops.simpleinterest;
+
+class SimpleInterest{
+    private Bigdecimal principal;
+
+    public String getPrincipal() {
+        return principal;
+    }
+
+    public void setPrincipal(String principal) {
+        this.principal = principal;
+    }
+
+    public String getInterest() {
+        return interest;
+    }
+
+    public void setInterest(String interest) {
+        this.interest = interest;
+    }
+
+    private String interest;
+    public SimpleInterest(String principal, String interest) {
+        this.principal = principal;
+        this.interest = interest;
+    }
+
+    void calculateTotalValue(int 5){
+
+
+    }
+
+}
