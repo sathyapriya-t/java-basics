3649cc8a68bb054a7f980a001c66dc624db2f5b0
sathya.Priya <sathya2001t@gmail.com>
2024-07-21 23:34:58 +0530

    Formatted

--- java-basics/src/java8/functionalinterface/PredicateNot.java
+++ java-basics/src/java8/functionalinterface/PredicateNot.java
@@ -6,13 +6,13 @@ import java.util.function.Predicate;
 public class PredicateNot {
 
 	public static void main(String[] args) {
-		List<Integer> list = List.of(1,2,3,4,5,6,7,8,9,10);
-		
-		//negate()
+		List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+		// negate()
 		System.out.println("isEven using negate() method:");
 		predicateNotWithNegateMethod(list);
-		
-		//Prediacte.not()
+
+		// Prediacte.not()
 		System.out.println("isEven using Predicate.not(predicate) method:");
 		predicateNotWithPredicateNotMethod(list);
 	}
@@ -21,12 +21,13 @@ public class PredicateNot {
 		Predicate<Integer> predicate = (num) -> isEven(num);
 		list.stream().filter(predicate.negate()).forEach(System.out::println);
 	}
+
 	private static void predicateNotWithPredicateNotMethod(List<Integer> list) {
 		list.stream().filter(Predicate.not(PredicateNot::isEven)).forEach(System.out::println);
 	}
 
 	private static boolean isEven(Integer num) {
-		return num%2==0;
+		return num % 2 == 0;
 	}
 
 }
