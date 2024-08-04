bc746c4a978b15ba9a3b5bc6edffdf21a4bbc3f9
sathya.Priya <sathya2001t@gmail.com>
2024-08-04 22:06:34 +0530

    Basics - Ternary Operator

---
+++ java-basics/src/collections/basics/TernaryOperator.java
@@ -0,0 +1,37 @@
+package collections.basics;
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+
+		// Ternary Operator - condition ? statement1: satement 2
+		int n1 = 10;
+		int n2 = 20;
+		String result = n1 < n2 ? n2 + " is greater" : "No";
+		System.out.println(result + "main 1");
+
+	}
+	
+	public static void main(String arg) {
+
+		// Ternary Operator - condition ? statement1: satement 2
+		int n1 = 10;
+		int n2 = 20;
+		String result = n1 < n2 ? n2 + " is greater" : "No";
+		System.out.println(result);
+
+	}
+	
+	public static void main() {
+
+		// Ternary Operator - condition ? statement1: satement 2
+		int n1 = 10;
+		int n2 = 20;
+		String result = n1 < n2 ? n2 + " is greater" : "No";
+		System.out.println(result);
+
+	}
+
+
+
+}
--- java-basics/src/problem/basics/RotateTheArrayOfInteger.java
+++ java-basics/src/problem/basics/RotateTheArrayOfInteger.java
@@ -5,64 +5,68 @@ import java.util.Arrays;
 public class RotateTheArrayOfInteger {
 
 	public static void main(String[] args) {
-		
-		int[] intArray = {1,2,3,4,6};
+
+		int[] intArray = { 1, 2, 3, 4, 6 };
+		// Just swapping the near lelements
 		usingTempAndSwap(intArray);
-		
+
+		// Swpping with temp and odd even values of Array
 		usingTempAndSwapingFirstLastValue(intArray);
-		int len=intArray.length;
+
+		// shifting from the specific position and New Array
+		// First we will loop through the elements from the position to end of the array
+		// and add it to the new temp array
+		// Next we will decrement from the position and it to the new Array
+		int len = intArray.length;
 		int position = 2;
-		
 		usingNewArrayAndStoringValues(intArray, len, position);
-		
 
 	}
 
 	private static void usingNewArrayAndStoringValues(int[] intArray, int len, int position) {
 		int[] tempArray = new int[len];
 		int cout = 0;
-		for(int i = position-1; i< len;i++) {
+		for (int i = position - 1; i < len; i++) {
 			tempArray[cout] = intArray[i];
 			cout++;
 		}
-		
-		for(int i = 0; i< position-1;i++) {
+
+		for (int i = 0; i < position - 1; i++) {
 			tempArray[cout] = intArray[i];
 			cout++;
 		}
-		
+
 		System.out.println(Arrays.toString(tempArray));
 	}
 
 	private static void usingTempAndSwapingFirstLastValue(int[] intArray) {
-		int len=intArray.length;
-		if(len%2==0) {
+		int len = intArray.length;
+		if (len % 2 == 0) {
 			int temp = 0;
-			for(int i =0; i < (len/2) -1; i++) {
+			for (int i = 0; i < (len / 2) - 1; i++) {
 				temp = intArray[i];
-				intArray[i] = intArray[len -i];
-				intArray[len -i] = temp;
+				intArray[i] = intArray[len - i];
+				intArray[len - i] = temp;
 			}
-			
-		}
-		else {
+
+		} else {
 			int temp = 0;
-			for(int i =0; i < ((len/2)-2); i++) {
+			for (int i = 0; i < ((len / 2) - 2); i++) {
 				temp = intArray[i];
-				intArray[i] = intArray[len -i];
-				intArray[len -i] = temp;
+				intArray[i] = intArray[len - i];
+				intArray[len - i] = temp;
 			}
-			
+
 		}
 		System.out.println(Arrays.toString(intArray));
 	}
 
 	private static void usingTempAndSwap(int[] intArray) {
 		int temp = 0;
-		for(int i =0; i < intArray.length-1; i++) {
+		for (int i = 0; i < intArray.length - 1; i++) {
 			temp = intArray[i];
-			intArray[i] = intArray[i+1];
-			intArray[i+1] = temp;
+			intArray[i] = intArray[i + 1];
+			intArray[i + 1] = temp;
 		}
 		System.out.println(Arrays.toString(intArray));
 	}
