5429b11655c5bae0872c50c9bcc15e8810f8e307
sathyapriya-t <sathya2001t@gmail.com>
2024-08-12 23:46:26 +0530

    Java21 - StringFormat

---
+++ java-basics/src/java21/StringFormat.java
@@ -0,0 +1,24 @@
+package java21;
+
+import data.Student;
+import data.StudentDataBase;
+
+import java.util.function.Supplier;
+
+import static java.lang.StringTemplate.STR;
+import static java.util.FormatProcessor.FMT;
+
+public class StringFormat {
+
+    public static void main(String[] args) {
+        Supplier<Student> studentList = StudentDataBase.studentSuplier;
+        Student student = studentList.get();
+
+        //Other string formatters are hard to read
+        //STR." string \{value}";
+        //FMT." string %.2f\{value}"; -> decimal formatting
+
+        //StringFormat stringFormat = STR. "This is \{student.getName()}";
+        //StringFormat stringFormat1 = FMT. "This is %.2F\{student.getName()}";
+    }
+}
