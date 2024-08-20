96ccec373da9a0bbf72561c30840bb895042d17d
sathya.Priya <sathya2001t@gmail.com>
2024-08-20 12:31:06 +0530

    Final Finally Finalize

--- java-basics/src/tips/FinalFinallyFinalize.java
+++ java-basics/src/tips/FinalFinallyFinalize.java
@@ -1,9 +1,39 @@
 package tips;
 
+final class FinalClas {
+	// final variable value cannot be changed
+	final int i = 10;
+
+	// final method can't be overridden
+	final void print() {
+		System.out.println(i);
+	}
+
+}
+
+//Gives compailation error because final calls can't be inherited
+//class InheritingFinalClass extends FinalClass{
+//	
+// final method can't be overriden
+//@override
+//void print() {
+//	System.out.println(i);
+//}
+//}
 public class FinalFinallyFinalize {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		FinalClas finalClass = new FinalClas();
+		try {
+			finalClass.print();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally{
+			System.out.println("Allways executes");
+		}
+		
 
 	}
 
